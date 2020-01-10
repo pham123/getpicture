cd9f6efe283270e77bf0b348645f4f72ca2efd66
pham123 <phamdong88@gmail.com>
2020-01-10 10:24:26 +0700

    update

--- cr.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'config.php';
-getfunc('db');
-$oDB=new db();
-$sql = "INSERT INTO link (`LinkName`,`LinkOption`) VALUES (?,?)";
-for ($i=0; $i < 2000 ; $i++) { 
-    $oDB->query($sql,'Name'.$i,0);
-}
